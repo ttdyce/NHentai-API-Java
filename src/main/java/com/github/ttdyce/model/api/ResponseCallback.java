575e1eef51c582d256b3c419d3a176647db2ed9a
afterDarkInvalid <108542922+afterDarkInvalid@users.noreply.github.com>
2022-07-08 22:26:22 -0700

    Update ResponseCallback.java

--- src/main/java/com/github/ttdyce/model/api/ResponseCallback.java
+++ src/main/java/com/github/ttdyce/model/api/ResponseCallback.java
@@ -2,4 +2,5 @@ package com.github.ttdyce.model.api;
 
 public abstract class ResponseCallback {
     public abstract void onReponse(String response);
+    public abstract void onError(String error);
 }
